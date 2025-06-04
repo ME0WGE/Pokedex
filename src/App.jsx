f7b9a870b1a9c66e641210b68a0322f80df205e7
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-04 15:43:19 +0200

    add components & pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [name, setName] = useState();
+
+  useEffect(() => {
+    axios
+      .get("https://pokebuildapi.fr/api/v1/pokemon")
+      .then((res) => setName(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
-      {/* Ready to code! */}
-      {/* Script written by github.com/ME0WGE */}
+      <></>
     </>
   );
 }
