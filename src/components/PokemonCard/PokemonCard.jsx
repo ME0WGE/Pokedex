05ee6a634fc06754e9f29192cb6036102617a015
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-04 17:52:39 +0200

    add PokemonCard & css, Home & css; display cards with name, type, id, image

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,5 +1,27 @@
 import "./pokemonCard.css";
+import { Link } from "react-router-dom";
 
-export default function PokemonCard() {
-  return;
+export default function PokemonCard({ pokemon }) {
+  return (
+    <div className="pokemon-card">
+      <div className="pokemon-card-image">
+        <img src={pokemon.image} alt={pokemon.name} loading="lazy" />
+        {/* Loading="lazy" permet de ne pas charger les images immédiatement, et de les charger seulement lorsqu'on s'approche de leurs cards */}
+      </div>
+      <div className="pokemon-card-info">
+        <h3 className="pokemon-name">{pokemon.name}</h3>
+        <div className="pokemon-id">#{pokemon.id}</div>
+        <div className="pokemon-types">
+          {pokemon.apiTypes.map((type, index) => (
+            <span key={index} className="pokemon-type">
+              {type.name}
+            </span>
+          ))}
+        </div>
+        <Link to={`/pokemon/${pokemon.id}`} className="details-button">
+          Plus de détails
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import "./home.css";
+import PokemonCard from "../../components/PokemonCard/PokemonCard";
+import axios from "axios";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Call API
+  useEffect(() => {
+    axios
+      .get("https://pokebuildapi.fr/api/v1/pokemon")
+      .then((response) => setPokemons(response.data), setLoading(false))
+      .catch((error) => console.log(`Error : ${error}`, setError(error)));
+  }, []);
+
+  // Filtrer les pokémons selon l'input de l'utilisateur
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <div className="home-container">
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
+
+      {/* Si les données sont chargées et qu'il n'y a pas d'erreur alors on affiche le contenu */}
+      {!loading && !error && (
+        <div className="pokemon-grid">
+          {filteredPokemons.length > 0 ? (
+            filteredPokemons.map((pokemon) => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
+            ))
+          ) : (
+            <div className="no-results">
+              <p>Aucun Pokémon ne correspond à votre recherche</p>
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
