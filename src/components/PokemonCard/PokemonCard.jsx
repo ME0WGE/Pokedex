510b0adb6de03fb5a4cae33b431d52cf33d22334
Kamil Baldyga <baldygakamil@gmail.com>
2025-06-04 22:09:43 +0200

    add colored types in the cards

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -2,21 +2,55 @@ import "./pokemonCard.css";
 import { Link } from "react-router-dom";
 
 export default function PokemonCard({ pokemon }) {
+  // Fonction pour s'assurer que la couleur est bien définie
+  const getTypeColor = (type) => {
+    if (type && type.color) {
+      return type.color;
+    }
+
+    // Couleurs de secours basées sur le nom du type
+    const typeColors = {
+      Normal: "#A8A878",
+      Feu: "#F08030",
+      Eau: "#6890F0",
+      Électrik: "#F8D030",
+      Plante: "#78C850",
+      Glace: "#98D8D8",
+      Combat: "#C03028",
+      Poison: "#A040A0",
+      Sol: "#E0C068",
+      Vol: "#A890F0",
+      Psy: "#F85888",
+      Insecte: "#A8B820",
+      Roche: "#B8A038",
+      Spectre: "#705898",
+      Dragon: "#7038F8",
+      Ténèbres: "#705848",
+      Acier: "#B8B8D0",
+      Fée: "#EE99AC",
+    };
+
+    return typeColors[type.name] || "#777777"; // Gris par défaut
+  };
+
   return (
     <div className="pokemon-card">
       <div className="pokemon-card-image">
         <img src={pokemon.image} alt={pokemon.name} loading="lazy" />
-        {/* Loading="lazy" permet de ne pas charger les images immédiatement, et de les charger seulement lorsqu'on s'approche de leurs cards */}
       </div>
       <div className="pokemon-card-info">
         <h3 className="pokemon-name">{pokemon.name}</h3>
         <div className="pokemon-id">#{pokemon.id}</div>
         <div className="pokemon-types">
-          {pokemon.apiTypes.map((type, index) => (
-            <span key={index} className="pokemon-type">
-              {type.name}
-            </span>
-          ))}
+          {pokemon.apiTypes &&
+            pokemon.apiTypes.map((type, index) => (
+              <span
+                key={index}
+                className="pokemon-type"
+                style={{ backgroundColor: getTypeColor(type) }}>
+                {type.name}
+              </span>
+            ))}
         </div>
         <Link to={`/pokemon/${pokemon.id}`} className="details-button">
           Plus de détails
